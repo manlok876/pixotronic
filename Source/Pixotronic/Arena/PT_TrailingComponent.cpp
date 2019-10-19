a4dce5958dd6de05bcfa0793db2d893951231f57
Denis Maximenko <manlok71@gmail.com>
2019-10-19 13:29:32 +0300

    Implement trailing logic
    Signed-off-by: Denis Maximenko <manlok71@gmail.com>

--- Source/Pixotronic/Arena/PT_TrailingComponent.cpp
+++ Source/Pixotronic/Arena/PT_TrailingComponent.cpp
@@ -4,7 +4,7 @@
 #include "PT_TrailingComponent.h"
 
 #include "UObject/ConstructorHelpers.h"
-#include "DrawDebugHelpers.h"
+#include "Kismet/KismetMathLibrary.h"
 #include "Materials/MaterialInstanceDynamic.h"
 
 
@@ -15,10 +15,20 @@ UPT_TrailingComponent::UPT_TrailingComponent()
 	PrimaryComponentTick.bCanEverTick = true;
 
 	TrailWidth = 20.0f;
-	Color = FColor(255, 255, 255);
-	CurrentInstance = -1;
+	TrailHeight = 100.0f;
+	TrailStartOffset = FVector(0.0f);
 
-	// TODO: create material
+	Color = FColor(255, 255, 255);
+
+	bIsTrailingEnabled = false;
+	CurrentInstance = -1;
+	
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> TrailMesh(TEXT("/Game/Assets/Meshes/Cube.Cube"));
+	if (TrailMesh.Succeeded())
+	{
+		SetStaticMesh(TrailMesh.Object);
+	}
+
 	static ConstructorHelpers::FObjectFinder<UMaterial> TrailMaterial(TEXT("/Game/Assets/Materials/TrailMaterial.TrailMaterial"));
 	if (TrailMaterial.Succeeded())
 	{
@@ -30,18 +40,44 @@ void UPT_TrailingComponent::BeginPlay()
 {
 	Super::BeginPlay();
 	SetColor(Color);
+	TurnOn();
+	// Maybe use native delegate?
+	//GetOwner()->GetRootComponent()->TransformUpdated.AddUObject(this, &UPT_TrailingComponent::OnTransformUpdated);
 }
-
 
 void UPT_TrailingComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-	// TODO: replace with InstancedMesh-based logic
-	DrawDebugLine(GetWorld(),
-		GetComponentLocation(),
-		GetComponentLocation() + FVector(0, 0, 100),
-		FColor(0, 255, 0),
-		true, -1, 0, 10);
+	UpdateTrail();
+}
+
+void UPT_TrailingComponent::OnTransformUpdated(USceneComponent* Component, EUpdateTransformFlags UpdateFlags, ETeleportType TeleportType)
+{
+	static FRotator PreviousRotation = FRotator::ZeroRotator;
+	UpdateTrail();
+	if (PreviousRotation != Component->GetComponentRotation())
+	{
+		MakeTurnpoint();
+	}
+}
+
+FTransform UPT_TrailingComponent::CalcNewSegmentTransform()
+{
+	FVector LastTurnPoint = TurnPoints[TurnPoints.Num() - 1];
+	FVector NewLocation = (LastTurnPoint + GetTrailStartLocation()) / 2.0f;
+	FRotator NewRotation = UKismetMathLibrary::MakeRotFromX(NewLocation - LastTurnPoint);
+	FVector NewScale = FVector(FVector::Distance(GetTrailStartLocation(), LastTurnPoint), TrailWidth, TrailHeight) / 100.0f;
+	return FTransform(NewRotation, NewLocation, NewScale);
+}
+
+void UPT_TrailingComponent::UpdateTrail()
+{
+	if (bIsTrailingEnabled) {
+		UpdateInstanceTransform(
+			CurrentInstance, 
+			CalcNewSegmentTransform(),
+			true, true);
+	}
 }
 
 void UPT_TrailingComponent::SetColor(const FColor& NewColor)
@@ -57,13 +93,69 @@ void UPT_TrailingComponent::SetColor(const FColor& NewColor)
 
 void UPT_TrailingComponent::MakeTurnpoint()
 {
+	if (bIsTrailingEnabled) 
+	{
+		EndCurrentSegment();
+		StartNewSegment();
+	}
 }
 
 void UPT_TrailingComponent::TurnOn()
 {
+	if (bIsTrailingEnabled)
+	{
+		return;
+	}
+	bIsTrailingEnabled = true;
+	MakeTurnpoint();
 }
 
 void UPT_TrailingComponent::TurnOff()
 {
+	if (!bIsTrailingEnabled)
+	{
+		return;
+	}
+	EndCurrentSegment();
+	bIsTrailingEnabled = false;
 }
 
+FVector UPT_TrailingComponent::GetTrailStartLocation()
+{
+	return GetOwner()->GetActorLocation() + TrailStartOffset;
+}
+
+void UPT_TrailingComponent::EndCurrentSegment()
+{
+	if (bIsTrailingEnabled && CurrentInstance >= 0)
+	{
+		check(CurrentInstance < GetInstanceCount());
+		UpdateTrail();
+		AddTurnPoint();
+		CurrentInstance = -1;
+	}
+}
+
+void UPT_TrailingComponent::StartNewSegment()
+{
+	int NewInstance =
+		AddInstance(
+			FTransform(
+				FRotator::ZeroRotator,
+				GetOwner()->GetActorLocation(),
+				FVector(0, 0, 0)));
+	if (NewInstance >= 0)
+	{
+		check(NewInstance > CurrentInstance);
+		if (TurnPoints.Num() == 0)
+		{
+			AddTurnPoint();
+		}
+		CurrentInstance = NewInstance;
+	}
+}
+
+void UPT_TrailingComponent::AddTurnPoint()
+{
+	TurnPoints.Add(GetOwner()->GetActorLocation());
+}
--- Source/Pixotronic/Arena/PT_TrailingComponent.h
+++ Source/Pixotronic/Arena/PT_TrailingComponent.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Components/SceneComponent.h"
+#include "Components/ActorComponent.h"
 #include "Components/InstancedStaticMeshComponent.h"
 #include "PT_TrailingComponent.generated.h"
 
@@ -22,6 +22,9 @@ public:
 	float TrailWidth;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float TrailHeight;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	FColor Color;
 
 	UFUNCTION(BlueprintCallable)
@@ -31,22 +34,47 @@ public:
 	void MakeTurnpoint();
 
 	// Enables trailing
-	UFUNCTION()
+	UFUNCTION(BlueprintCallable)
 	void TurnOn();
 
 	// Disables trailing
-	UFUNCTION()
+	UFUNCTION(BlueprintCallable)
 	void TurnOff();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FVector TrailStartOffset;
+
+	UFUNCTION(BlueprintPure)
+	FVector GetTrailStartLocation();
 
 protected:
 	virtual void BeginPlay() override;
 
 	static const FName ColorParameterName;
 
+	UFUNCTION()
+	void UpdateTrail();
+
+	void OnTransformUpdated(USceneComponent* Component, EUpdateTransformFlags UpdateFlags, ETeleportType TeleportType);
+
 private:
+	UPROPERTY()
+	bool bIsTrailingEnabled;
+
 	UPROPERTY()
 	TArray<FVector> TurnPoints;
 
 	UPROPERTY()
 	int CurrentInstance;
+
+	UFUNCTION()
+	FTransform CalcNewSegmentTransform();
+
+	UFUNCTION()
+	void EndCurrentSegment();
+	UFUNCTION()
+	void StartNewSegment();
+
+	UFUNCTION()
+	void AddTurnPoint();
 };
