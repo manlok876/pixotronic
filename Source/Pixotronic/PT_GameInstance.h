9162403b3c7a3ba1272b84d16df24cd6f763ecc8
Denis Maximenko <manlok71@gmail.com>
2019-10-20 12:29:28 +0300

    Add PT_GameInstance class
    Signed-off-by: Denis Maximenko <manlok71@gmail.com>

---
+++ Source/Pixotronic/PT_GameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Core.h"
+#include "Engine/GameInstance.h"
+#include "PT_GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PIXOTRONIC_API UPT_GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+};
