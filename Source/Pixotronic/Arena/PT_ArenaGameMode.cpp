bda152266dfc1263a8be9139df906891cc8b3886
Denis Maximenko <manlok71@gmail.com>
2019-10-25 01:26:36 +0300

    Make a draft of main game loop
    Signed-off-by: Denis Maximenko <manlok71@gmail.com>

--- Source/Pixotronic/Arena/PT_ArenaGameMode.cpp
+++ Source/Pixotronic/Arena/PT_ArenaGameMode.cpp
@@ -26,6 +26,7 @@ AActor* APT_ArenaGameMode::ChoosePlayerStart_Implementation(AController* Player)
 {
 	for (TActorIterator<APlayerStart> StartItr(GetWorld()); StartItr; ++StartItr)
 	{
+		// Need to actually choose start point here
 		break;
 	}
 	return Super::ChoosePlayerStart_Implementation(Player);
@@ -38,26 +39,47 @@ bool APT_ArenaGameMode::ReadyToStartMatch_Implementation()
 
 void APT_ArenaGameMode::HandleMatchHasStarted()
 {
-	//for (TActorIterator<APlayerController> PCItr(GetWorld()); PCItr; ++PCItr)
-	//{
-	//	PCItr->SetPause(true);
-	//}
 	Super::HandleMatchHasStarted();
+	
+	// Maybe pause all controllers and resume after delay
 }
 
 bool APT_ArenaGameMode::ReadyToEndMatch_Implementation()
 {
-	//return GetGameState<APT_ArenaGameState>();
-	return false;
+	return GetGameState<APT_ArenaGameState>()->GetNumAlivePlayers() <= 1;
 }
 
 void APT_ArenaGameMode::HandleMatchHasEnded()
 {
-	APT_ArenaGameState* CastGameState = GetGameState<APT_ArenaGameState>();
+	APT_ArenaGameState* ArenaGameState = GetGameState<APT_ArenaGameState>();
+	
+	// Add points to surviving players, if any
+
+	if (ArenaGameState->GetMaxPlayerScore() < WinningScore)
+	{
+		RestartMatch();
+	}
+	else 
+	{
+		// Do nothing, maybe display a message
+	}
 }
 
 void APT_ArenaGameMode::RestartMatch()
 {
+	SetMatchState(MatchState::WaitingToStart);
+	for (TActorIterator<APlayerController> PCItr(GetWorld()); PCItr; ++PCItr)
+	{
+		APawn* PlayerPawn = PCItr->GetPawn();
+		if (IsValid(PlayerPawn))
+		{
+			// Destroy pawn so that it is re-created at start of new round
+			// to avoid messing with
+			//PlayerPawn->Destroy();
+			//PCItr->SetPawn(nullptr);
+		}
+	}
+	StartMatch();
 }
 
 void APT_ArenaGameMode::SetMaxPlayers(int NewMaxPlayers)
--- Source/Pixotronic/Arena/PT_ArenaGameMode.h
+++ Source/Pixotronic/Arena/PT_ArenaGameMode.h
@@ -35,4 +35,6 @@ private:
 	
 	float RoundStartDelay;
 	FTimerHandle RoundStartTimer;
+
+	int WinningScore;
 };
