85149440e8c64ab90d121681806b5e2401d8d1b3
Denis Maximenko <manlok71@gmail.com>
2019-10-26 09:58:08 +0300

    Create player state class and color repication
    Signed-off-by: Denis Maximenko <manlok71@gmail.com>

---
+++ Source/Pixotronic/Arena/PT_ArenaPlayerState.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PT_ArenaPlayerState.h"
+
+#include "UnrealNetwork.h"
+
+void APT_ArenaPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(APT_ArenaPlayerState, PlayerColor);
+}
+
+void APT_ArenaPlayerState::SetColor_Implementation(const FColor& NewColor)
+{
+	PlayerColor = NewColor;
+}
+
+bool APT_ArenaPlayerState::SetColor_Validate(const FColor& NewColor)
+{
+	return true;
+}
---
+++ Source/Pixotronic/Arena/PT_ArenaPlayerState.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "PT_ArenaPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PIXOTRONIC_API APT_ArenaPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(Replicated)
+	FColor PlayerColor;
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void SetColor(const FColor& NewColor);
+};
