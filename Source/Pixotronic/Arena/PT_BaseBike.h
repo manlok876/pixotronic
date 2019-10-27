a4a064c72fea0e69de294a8cefe1457f01f85100
Denis Maximenko <manlok71@gmail.com>
2019-10-28 01:25:20 +0300

    Rename PlayerColor to BikeColor
    Signed-off-by: Denis Maximenko <manlok71@gmail.com>

--- Source/Pixotronic/Arena/PT_BaseBike.h
+++ Source/Pixotronic/Arena/PT_BaseBike.h
@@ -30,7 +30,7 @@ public:
 
 	// Color used for bike and its trail
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	FLinearColor PlayerColor;
+	FLinearColor BikeColor;
 
 	UFUNCTION(Server, Reliable, WithValidation)
 	void TurnLeft();
