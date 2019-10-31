5bc788d430f41e592522a4fcbd3d93025b259cd9
Denis Maximenko <manlok71@gmail.com>
2019-10-31 18:47:56 +0300

    Revert "Add readiness flag to startup controller"
    This reverts commit 60c1f1b7f1ddae53486714baf79f12e66bf1f376.

--- Source/Pixotronic/Startup/PT_StartupPlayerController.cpp
+++ Source/Pixotronic/Startup/PT_StartupPlayerController.cpp
@@ -3,22 +3,3 @@
 
 #include "PT_StartupPlayerController.h"
 
-APT_StartupPlayerController::APT_StartupPlayerController()
-{
-	ReadyToStart = false;
-}
-
-void APT_StartupPlayerController::SetReadyToStart_Implementation(bool IsReady)
-{
-	ReadyToStart = IsReady;
-}
-
-bool APT_StartupPlayerController::SetReadyToStart_Validate(bool IsReady)
-{
-	return true;
-}
-
-bool APT_StartupPlayerController::GetReadyToStart()
-{
-	return ReadyToStart;
-}
