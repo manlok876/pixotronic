475cfdf7b3e974ff66c7860531c4678001a6a472
Denis Maximenko <manlok71@gmail.com>
2019-11-08 03:58:47 +0300

    Expose delegate for forwarding android back press in startup controller
    Signed-off-by: Denis Maximenko <manlok71@gmail.com>

--- Source/Pixotronic/Startup/PT_StartupPlayerController.cpp
+++ Source/Pixotronic/Startup/PT_StartupPlayerController.cpp
@@ -3,3 +3,14 @@
 
 #include "PT_StartupPlayerController.h"
 
+void APT_StartupPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	InputComponent->BindAction("Return", IE_Pressed, this, &APT_StartupPlayerController::HandleBackPressed);
+}
+
+void APT_StartupPlayerController::HandleBackPressed()
+{
+	OnBackButtonPressed.Broadcast();
+}
--- Source/Pixotronic/Startup/PT_StartupPlayerController.h
+++ Source/Pixotronic/Startup/PT_StartupPlayerController.h
@@ -6,6 +6,8 @@
 #include "GameFramework/PlayerController.h"
 #include "PT_StartupPlayerController.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnBackButtonPressedDelegate);
+
 /**
  * 
  */
@@ -13,5 +15,14 @@ UCLASS()
 class PIXOTRONIC_API APT_StartupPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void SetupInputComponent() override;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnBackButtonPressedDelegate OnBackButtonPressed;
+
+private:
+	UFUNCTION()
+	void HandleBackPressed();
 };
